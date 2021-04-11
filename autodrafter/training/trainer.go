48a0710b308fc3b759938f11c079958a922a0f45
Jake Leroux <31752192+Answerious@users.noreply.github.com>
2021-04-11 17:34:15 -0600

    Add files via upload

---
+++ autodrafter/training/trainer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+)
+
+func copyOutput(r io.Reader){
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan(){
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main(){
+	cmd := exec.Command("python", "autodrafter/training/data.py", "--input-file")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil{
+		log.Fatal(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil{
+		log.Fatal(err)
+	}
+	err = cmd.Start()
+	if err != nil{
+		log.Fatal(err)
+	}
+	go copyOutput(stdout)
+	go copyOutput(stderr)
+	cmd.Wait()
+}
