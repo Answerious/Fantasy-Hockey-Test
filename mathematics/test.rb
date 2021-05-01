95fbbe60919d0c9ce1cf3ead7f366a098ceabb07
Jake Leroux <31752192+Answerious@users.noreply.github.com>
2021-05-01 02:14:57 -0600

    Update test.rb

--- mathematics/test.rb
+++ mathematics/test.rb
@@ -1,7 +1,7 @@
-/
-PLAYER STAT GENERATOR
-FYI this is very broken and doesn't work often
-/
+#/
+#PLAYER STAT GENERATOR
+#FYI this is very broken and doesn't work often
+#/
 
 class Sort
     def initialize(file)
@@ -13,12 +13,12 @@ class Sort
     end
 end
 
-/
-TODO -- Overall - > Accuracy, Power, Strengh 
-An Overall will be each substat added and divided by 3
-Random numbers for each one so it could be 83 OVR but
-Accuracy = 87, Power = 80, Strengh = 82
-/
+#/
+#TODO -- Overall - > Accuracy, Power, Strengh 
+#An Overall will be each substat added and divided by 3
+#Random numbers for each one so it could be 83 OVR but
+#Accuracy = 87, Power = 80, Strengh = 82
+#/
 
 class Stats
     def initialize(file)
@@ -31,15 +31,15 @@ class Stats
             (10.times { a = (rand(1..3)) and nums << a } and array = nums.uniq) and 3.times{ @start << overall } 
             add, sub1, sub2 = array.index { |x| [3].include?(x) }, array.index { |x| [1].include?(x) }, array.index { |x| [2].include?(x) }
             rand(1..5).times { (final << (@start[add]+3) and final << (@start[sub1]-1) and final << (@start[sub2]-2)) and @start = final and final = []}
-            send<<@names[0] and send<<@start and@overall.delete_at(0) and @names.delete_at(0) and i += 1 and @fin<<send
+            (send<<@names[0] and send<<@start and@overall.delete_at(0) and @names.delete_at(0) and i += 1 and @fin<<send)
         end
         return (@fin)
     end
 end
 
-/
-Saving player data
-/
+#/
+#Saving player data
+#/
 
 class Saving
     def initialize(data)
